c3967f425d97e206a4f07adc4ab38a5cfef459aa
F.M. Panggabean <cichiomaru@users.noreply.github.com>
2022-06-02 15:38:19 +0700

    penambahan AI
    penambahan AI patroli

---
+++ Assets/Apps/Scripts/AI/PigAI.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EntityBehaviour;
+
+namespace AI {
+    public class PigAI : MonoBehaviour {
+        /// <summary>
+        /// state dari AI
+        /// </summary>
+        [SerializeField]
+        private State state;
+        /// <summary>
+        /// action patroli
+        /// </summary>
+        [SerializeField]
+        private Patrol patrol;
+
+        /// <summary>
+        /// reference ke komponen moveable
+        /// </summary>
+        private MoveAction moveable;
+
+        private void Awake() {
+            moveable = GetComponent<MoveAction>();
+        }
+        private void Start() {
+
+        }
+        private void Update() {
+            if (state == State.Patrol) {
+                Patrol();
+            } else if (state == State.Chase) {
+                Chase();
+            }
+        }
+
+        /// <summary>
+        /// kejar musuh jika ada
+        /// </summary>
+        private void Chase() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// patroli
+        /// </summary>
+        private void Patrol() {
+            if (patrol == null) return;
+            if (patrol.GetWaypointsCount() <= 0) return;
+
+            moveable.SetDirection(patrol.GetDirection(transform.position), Axis.x);
+        }
+    } 
+}
